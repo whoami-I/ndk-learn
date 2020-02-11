35043bfa8ec8ce035bb82d6a93178d732a698534
haigen.zhu <haigen.zhu@tcl.com>
2020-02-11 19:06:13 +0800

    aaa

--- app/src/main/cpp/hello.cpp
+++ app/src/main/cpp/hello.cpp
@@ -3,8 +3,17 @@
 //
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
-extern "C" JNIEXPORT jstring
+#define TAG "projectname" // 这个是自定义的LOG的标识
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__) // 定义LOGD类型
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__) // 定义LOGI类型
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__) // 定义LOGW类型
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGE类型
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__) // 定义LOGF类型
+
+
+extern "C" JNIEXPORT jstring JNICALL
 Java_cn_hzw_ndk_1learn_MainActivity_getNdkText(
         JNIEnv *env,
         jobject /* this */) {
@@ -12,3 +21,31 @@ Java_cn_hzw_ndk_1learn_MainActivity_getNdkText(
     return env->NewStringUTF(hello.c_str());
 }
 
+extern "C"
+JNIEXPORT void JNICALL
+Java_cn_hzw_ndk_1learn_MainActivity_ndkPrint(JNIEnv *env, jobject thiz) {
+    jclass jclazz = env->GetObjectClass(thiz);
+    jmethodID methodId = env->GetMethodID(jclazz, "print", "()V");
+    env->CallVoidMethod(thiz, methodId);
+
+    jmethodID methodId2 = env->GetStaticMethodID(jclazz, "add", "(ID)V");
+    env->CallStaticVoidMethod(jclazz, methodId2, 1, 2.3);
+
+    jfieldID jfieldId = env->GetFieldID(jclazz, "aa", "I");
+    jint aa = env->GetIntField(thiz, jfieldId);
+    LOGD("aa=%d\n", aa);
+
+    jclass clz = env->FindClass("cn/hzw/ndk_learn/MainActivity");
+    if (clz == jclazz) {
+        LOGD("find class\n");
+    }
+    methodId2 = env->GetStaticMethodID(clz, "add", "(ID)V");
+    env->CallStaticVoidMethod(jclazz, methodId2, 1, 2.3);
+
+}extern "C"
+JNIEXPORT void JNICALL
+Java_cn_hzw_ndk_1learn_MainActivity_setString(JNIEnv *env, jobject thiz, jstring s) {
+    jboolean b = true;
+    const char *ss = env->GetStringUTFChars(s, &b);
+    LOGD("%s\n", ss);
+}
--- app/src/main/java/cn/hzw/ndk_learn/MainActivity.java
+++ app/src/main/java/cn/hzw/ndk_learn/MainActivity.java
@@ -3,9 +3,19 @@ package cn.hzw.ndk_learn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private int aa = 100;
+    private String s = "sss";
+
+    public String add(int a, double b, String c) {
+        return "sss";
+    }
 
     static {
         System.loadLibrary("hello");
@@ -17,7 +27,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView tv = findViewById(R.id.tv);
         tv.setText(getNdkText());
+        ndkPrint();
+        setString("llllllllll");
     }
 
     public native String getNdkText();
+
+    public native void ndkPrint();
+
+    public native void setString(String s);
+
+    public void print() {
+        Log.d(TAG, "hello world");
+    }
+
+    public static void add(int a, double b) {
+        Log.d(TAG, "" + (a + b));
+    }
 }
